c4ff7fe57e7a9e54a2f4256fd9cd63a2fe807e7b
Jeremi Niedziela <jeremi.niedziela@cern.ch>
2024-04-22 15:50:43 +0200

    Moving path to store pythia samples to the submission config.

--- utils/shift_produce_pythia_events.sh
+++ utils/shift_produce_pythia_events.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 user_base_path="/afs/desy.de/user/j/jniedzie/tea_shift/bin/"
-output_path="/nfs/dust/cms/user/jniedzie/shift/pythia_test_events/"
 python_path="/afs/desy.de/user/j/jniedzie/miniconda3/envs/tta/bin/python3"
 base_pythia_card="shift_base_pythia_card.cmnd"
 pythia_exec_path="/afs/desy.de/user/j/jniedzie/pythia_new/pythia8309/examples/main42"
@@ -17,4 +16,4 @@ export LD_LIBRARY_PATH=/cvmfs/sft.cern.ch/lcg/releases/gcc/8.3.0-cebb0/x86_64-ce
 cd $user_base_path || exit
 #mkdir -p output error log
 
-$python_path $user_base_path/shift_produce_pythia_events.py --part $1 --n_events $2 --m_z_prime $3 --m_dark_hadron $4 --m_dark_quark $5 --lifetime $6 --output_path $output_path --base_pythia_card $base_pythia_card --pythia_exec $pythia_exec_path
+$python_path $user_base_path/shift_produce_pythia_events.py --part $1 --n_events $2 --m_z_prime $3 --m_dark_hadron $4 --m_dark_quark $5 --lifetime $6 --output_path $7 --base_pythia_card $base_pythia_card --pythia_exec $pythia_exec_path
